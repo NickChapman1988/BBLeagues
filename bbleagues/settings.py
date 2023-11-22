77c661bf6aaddfde1eea475e740a6f846d11eecb
NickChapman1988 <muppetnj_uk@hotmail.com>
2023-11-22 15:54:34 +0000

    Copy allauth templates

--- bbleagues/settings.py
+++ bbleagues/settings.py
@@ -59,7 +59,10 @@ ROOT_URLCONF = 'bbleagues.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -142,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
