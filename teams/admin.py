52245607b2186c485e6c1cbb3da7febec9021198
NickChapman1988 <muppetnj_uk@hotmail.com>
2023-11-28 17:38:35 +0000

    Adjust admin panel table sorting

--- teams/admin.py
+++ teams/admin.py
@@ -3,5 +3,38 @@ from .models import Position, Team
 
 # Register your models here.
 
-admin.site.register(Position)
-admin.site.register(Team)
+class PositionAdmin(admin.ModelAdmin):
+    """ Admin for the Position model """
+    list_display = (
+        'position_name',
+        'team',
+        'cost',
+        'max_qty',
+        'ma',
+        'st',
+        'ag',
+        'pa',
+        'av',
+        'skills',
+        'primary_skills',
+        'secondary_skills',
+    )
+
+    ordering = ('team',)
+
+
+class TeamAdmin(admin.ModelAdmin):
+    """ Admin for the Team model """
+    list_display = (
+        'team',
+        'reroll_cost',
+        'special_rules',
+        'tier',
+        'apothecary',
+    )
+
+    ordering = ('team',)
+
+
+admin.site.register(Position, PositionAdmin)
+admin.site.register(Team, TeamAdmin)
