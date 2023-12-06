1cc4575c5f02814b52bc16e0b7f42cf86fc871a8
NickChapman1988 <muppetnj_uk@hotmail.com>
2023-12-06 21:58:14 +0000

    Create AddPlayerForm

--- teams/forms.py
+++ teams/forms.py
@@ -7,7 +7,6 @@ from .models import Team, Position, MemberTeam, Player
 class MemberTeamForm(forms.ModelForm):
     """ A form for new Member Teams """
 
-
     class Meta:
         """ Metadata for MemberTeamForm """
         model = MemberTeam
@@ -25,3 +24,28 @@ class MemberTeamForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+
+
+class AddPlayerForm(forms.ModelForm):
+    """ A form for adding new players to a member team """
+
+    class Meta:
+        """ Metadata for AddPlayerForm """
+        model = Player
+        fields = [
+            "player_name",
+            "player_no",
+            "position",
+        ]
+    
+    def __init__(self, *args, **kwargs):
+        """ init method for AddPlayerForm """
+        super().__init__(*args, **kwargs)
+        member_team = get_object_or_404(MemberTeam, id=team_id)
+        positions = Position.objects.filter(team=member_team)
+        players = [(p.id, p.position_name) for p in positions]
+
+        self.fields['position'].choices = players
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+            
